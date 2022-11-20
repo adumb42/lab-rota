ad2b8ecf0a20143b3e9c6b89b4e66b82559a3498
Adam Paul <adumb42@gmail.com>
2022-11-20 14:44:19 +0000

    201122

--- src/apis/holidays.js
+++ src/apis/holidays.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://lab-rota-api.herokuapp.com",
+    baseURL: "https://lab-rota-api.azurewebsites.net/",
     headers: {
         "Content-Type": "application/json"
     }
